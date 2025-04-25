69a635bdd9b563d55c2718c92c065180fec29b26
tgorsk15 <tgbusiness463@gmail.com>
2025-04-24 20:08:03 -0500

    add pandas for doc parsing

--- parseTool.py
+++ parseTool.py
@@ -1,11 +1,15 @@
-def parseDoc():
-    return 1
+# How file works
 
-print(parseDoc())
-
-# parse the data:
+# parse the data (potentially do this through pandas):
 # put coordinates in one array, put Unicode characters in another array
 # iterate through the length of either array, and place the Unicode character at the corresponding coordinates (both will have same index position)
 
 # create a "board", where the width is the highest x-coordinate found in the Doc
 # height will be the highest y-coordinate found
+
+def parseDoc():
+    return 1
+
+print(parseDoc())
+
+
