82e58d0396ddf7efa404d2b3f669578e50167818
tgorsk15 <tgbusiness463@gmail.com>
2025-04-24 19:21:33 -0500

    Psuedocode a plan

--- parseTool.py
+++ parseTool.py
@@ -2,3 +2,10 @@ def parseDoc():
     return 1
 
 print(parseDoc())
+
+# parse the data:
+# put coordinates in one array, put Unicode characters in another array
+# iterate through the length of either array, and place the Unicode character at the corresponding coordinates (both will have same index position)
+
+# create a "board", where the width is the highest x-coordinate found in the Doc
+# height will be the highest y-coordinate found
